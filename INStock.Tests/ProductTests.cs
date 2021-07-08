a9b77c9f0da329284b56d4efee4fcc55ac5a3d1c
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-07-08 11:28:32 +0100

    INStock product tests added

--- INStock.Tests/ProductTests.cs
+++ INStock.Tests/ProductTests.cs
@@ -1,9 +1,25 @@
 namespace INStock.Tests
 {
+    using INStock;
     using NUnit.Framework;
-
+    using System;
     public class ProductTests
     {
-        
+        [Test]
+        public void Constructor_WithValidParameters_ShouldSetFieldsCorrectly()
+        {
+            var product = new Product("wine", 8.99M);
+            Assert.AreEqual("wine", product.Name);
+            Assert.AreEqual(8.99M, product.Price);
+        }
+        [TestCase("", 19.98)]
+        [TestCase(" ", 12.76)]
+        [TestCase(null, 5.87)]
+        [TestCase("Magnum icecream", -5)]
+        [TestCase("Bubble gum", 0)]
+        public void Constructor_WithInvalidParameter_ShouldThrowArgumentException(string name, decimal price)
+        {
+            Assert.Throws<ArgumentException>(() => new Product(name, price));
+        }
     }
 }
---
+++ INStock/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace INStock
+{
+    public class Product
+    {
+        private string name;
+        private decimal price;
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Product name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Price 
+        {
+            get
+            {
+                return price;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Product price must be a positive number");
+                }
+                price = value;
+            }
+        }
+    }
+}
