80d4c250e10abfd8bd72ac2dc94feefbb559fe2e
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-07-08 13:56:04 +0100

    FakeTarget added

---
+++ FakeAxeAndDummy.Tests/FakeTarget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FakeAxeAndDummy.Tests
+{
+    internal class FakeTarget : ITarget
+    {
+        public int GiveExperience()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsDead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeAttack(int attackPoints)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
