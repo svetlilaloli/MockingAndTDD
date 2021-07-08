20fe18e4740aa2cbc4db20b97308458809573cfd
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-07-08 14:55:17 +0100

    FakeProduct class created

---
+++ INStock.Tests/FakeProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace INStock.Tests
+{
+    internal class FakeProduct : IProduct
+    {
+        internal FakeProduct()
+        {
+            Label = "Chocolate 100g";
+            Price = 1.9M;
+        }
+        internal FakeProduct(string label, decimal price)
+        {
+            Label = label;
+            Price = price;
+        }
+        public string Label { get; set; }
+        public decimal Price { get; set; }
+    }
+}
